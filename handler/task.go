9b17dc4956f30683ae4517a732fc0e8aeea7f7b7
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-10-17 22:43:44 +0200

    Insert

---
+++ handler/task.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"go-course/task"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type taskHandler struct {
+	taskService task.Service
+}
+
+func NewTaskHandler(taskService task.Service) *taskHandler {
+	return &taskHandler{taskService}
+}
+
+func (handler *taskHandler) Store(c *gin.Context) {
+	var input task.InputTask
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		response := Response{
+			Success: false,
+			Message: "something went wrong",
+			Data:    err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	newTask, err := handler.taskService.Store(input)
+	if err != nil {
+		response := Response{
+			Success: false,
+			Message: "something went wrong",
+			Data:    err.Error(),
+		}
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := Response{
+		Success: true,
+		Message: "new task successfully added",
+		Data:    newTask,
+	}
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"go-course/handler"
+	"go-course/task"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		dbURL = "user:password@tcp(127.0.0.1:3306)/go-course?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
+	db, err := gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	db.AutoMigrate(&task.Task{})
+
+	taskRepository := task.NewRepository(db)
+	taskService := task.NewService(taskRepository)
+	taskHandler := handler.NewTaskHandler(taskService)
+
+	r := gin.Default()
+	api := r.Group("/api")
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	api.POST("/task", taskHandler.Store)
+
+	r.Run(":3000")
+}
